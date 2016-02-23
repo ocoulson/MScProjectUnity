73b6ec1029f1fb20f3f2cf113e6ae4f8e4180b3a
ocoulson <o_coulson@hotmail.co.uk>
2016-02-23 14:36:29 +0000

    Made clouds destroy after exiting screen to avoid buildup

--- Assets/TitleAssets/Scripts/Cloud.cs
+++ Assets/TitleAssets/Scripts/Cloud.cs
@@ -4,13 +4,24 @@ using System.Collections;
 public class Cloud : MonoBehaviour {
 	float speed;
 
+	float xBound;
 	// Use this for initialization
 	void Start () {
 		speed = Random.Range(0.5f, 1.5f);
+
+		float zDistance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 cameraX = Camera.main.ViewportToWorldPoint(new Vector3(0,0,zDistance));
+
+		xBound = cameraX.x - 5f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		this.transform.position += Vector3.left * speed * Time.deltaTime;
+
+		if (transform.position.x < xBound) {
+			Destroy(gameObject);
+		}	
 	}
 }
