60e2eafb477b9fabb6db5fa5c391c2cfe5778977
ocoulson <o_coulson@hotmail.co.uk>
2016-04-19 00:03:33 +0100

    Added TextTyper class and UiManager for the MainMenu scene

---
+++ Assets/MenuAssets/Scripts/TextTyper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextTyper : MonoBehaviour {
+
+	public float letterPause = 0.01f;
+
+	string messageText;
+    Text textComp;
+	// Use this for initialization
+	void Start () {
+
+		textComp = GetComponent<Text>();
+        messageText = textComp.text;
+        textComp.text = "";
+        StartCoroutine(TypeText ());
+	}
+
+	IEnumerator TypeText () {
+         foreach (char letter in messageText.ToCharArray()) {
+             textComp.text += letter;
+             yield return new WaitForSeconds (letterPause);
+         }
+     }
+	
+
+}
---
+++ Assets/MenuAssets/Scripts/UiManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UiManager : MonoBehaviour {
+	float timer = 0f;
+	public GameObject canvas;
+	public GameObject[] text;
+	bool tDone = false;
+	bool ngDone = false;
+	bool cDone = false;
+	bool oDone = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		timer += Time.deltaTime;
+
+		if (timer >= 1f && !tDone) {
+			SpawnText(0);
+			tDone = true;
+		} else if (timer >= 2f && !ngDone) {
+			SpawnText(1);
+			ngDone = true;
+		} else if (timer >= 3f && !cDone) {
+			SpawnText(2);
+			cDone = true;
+		} else if (timer >= 4f && !oDone) {
+			SpawnText(3);
+			oDone = true;
+		}
+	}
+
+	void SpawnText (int i) {
+		GameObject newText = GameObject.Instantiate (text [i], text [i].transform.position, Quaternion.identity) as GameObject;
+		newText.transform.SetParent(canvas.transform, false);
+	}
+}
