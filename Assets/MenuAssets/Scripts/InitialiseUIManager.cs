6c2e48cb6065d10b5db3d867d5a5f546a7a888a6
ocoulson <o_coulson@hotmail.co.uk>
2016-06-13 17:40:49 +0100

    Added a new little scene between title and MainMenu and moved the Initialising text to that, rather than at the beginning of the MainMenu

---
+++ Assets/MenuAssets/Scripts/InitialiseUIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitialiseUIManager : MonoBehaviour {
+	float timer = 0f;
+	public GameObject text;
+	LevelManager levelManager;
+	// Use this for initialization
+	void Start () {
+		GameObject newObject = GameObject.Instantiate(text, text.transform.position,Quaternion.identity) as GameObject;
+		newObject.transform.SetParent(GameObject.FindObjectOfType<Canvas>().transform, false);
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+
+	void Update ()
+	{
+		timer += Time.deltaTime;
+		if (timer > 2f) {
+			levelManager.LoadNextLevel();
+		}
+	}
+
+
+}
--- Assets/MenuAssets/Scripts/MainMenuUiManager.cs
+++ Assets/MenuAssets/Scripts/MainMenuUiManager.cs
@@ -3,62 +3,27 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class MainMenuUiManager : MonoBehaviour {
-	float pause = 1f;
+	float pause = 0.6f;
 	public GameObject canvas;
 	public GameObject[] text;
-	bool initialised = false;
+
 
 
 	void Start ()
 	{
-		Debug.Log("1");
 		StartCoroutine(SpawnText());
-		//GameObject.Destroy(GameObject.FindObjectOfType<InitialiseType>().gameObject);
 
 	}
 
 	IEnumerator SpawnText ()
 	{
 		foreach (GameObject go in text) {
-			if (initialised) {
-				GameObject.Destroy(GameObject.FindObjectOfType<InitialiseType>().gameObject);
-				initialised = false;
-				pause = 0.5f;
-			}
+			
 			GameObject newText = GameObject.Instantiate (go, go.transform.position, Quaternion.identity) as GameObject;
-			if (go.Equals(text[0])) {
-				initialised = true;
-			}
 			newText.transform.SetParent(canvas.transform, false);
 			yield return new WaitForSeconds(pause);
 		}
 
 	}
-	// Update is called once per frame
-//	void Update ()
-//	{
-//
-//
-//		if (initialised) {
-//			
-//			timer += Time.deltaTime;
-//			if (timer >= 1f && !tDone) {
-//				GameObject.Destroy (initialisedText.gameObject);
-//				SpawnText(text[0]);
-//				tDone = true;
-//			} else if (timer >= 1.5f && !ngDone) {
-//				SpawnText(text[1]);
-//				ngDone = true;
-//			} else if (timer >= 2f && !cDone) {
-//				SpawnText(text[2]);
-//				cDone = true;
-//			} else if (timer >= 2.5f && !oDone) {
-//				SpawnText(text[3]);
-//				oDone = true;
-//			}
-//		}
-//
-//	}
-
 
 }
