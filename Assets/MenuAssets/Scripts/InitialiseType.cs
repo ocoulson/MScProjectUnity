c12354dcb55ecc47452a5ad5b784e33b71b26992
ocoulson <o_coulson@hotmail.co.uk>
2016-06-13 16:42:54 +0100

    Added a little 'intialising' message to the main menu, created the options menu. Now going to try to redo the main menu ui manager to use coroutines.

---
+++ Assets/MenuAssets/Scripts/InitialiseType.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InitialiseType : MonoBehaviour {
+
+	public float pause = 0.5f;
+	public bool finished = false;
+
+	string loadingDots = ".......";
+    Text textComp;
+    int textLength;
+
+	void Start () {
+
+		textComp = GetComponent<Text>();
+      	textLength = textComp.text.Length;
+        StartCoroutine(TypeText ());
+	}
+
+	IEnumerator TypeText () {
+         foreach (char letter in loadingDots.ToCharArray()) {
+             textComp.text += letter;
+             yield return new WaitForSeconds (pause);
+         }
+     }
+
+     void Update ()
+	{
+		if (textComp.text.Length == textLength + loadingDots.Length) {
+			finished = true;
+		} 
+     }
+	
+
+}
---
+++ Assets/MenuAssets/Scripts/MainMenuUiManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuUiManager : MonoBehaviour {
+	float timer = 0f;
+	public GameObject initialiseText;
+	public GameObject canvas;
+	public GameObject[] text;
+	InitialiseType initialisedText;
+	bool initialised = false;
+	bool tDone = false;
+	bool ngDone = false;
+	bool cDone = false;
+	bool oDone = false;
+
+	void Start() {
+		SpawnText(initialiseText);
+		initialisedText = GameObject.FindObjectOfType<InitialiseType>();
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!initialised) {
+			initialised = initialisedText.finished;
+
+		}
+
+		if (initialised) {
+			
+			timer += Time.deltaTime;
+			if (timer >= 1f && !tDone) {
+				GameObject.Destroy (initialisedText.gameObject);
+				SpawnText(text[0]);
+				tDone = true;
+			} else if (timer >= 1.5f && !ngDone) {
+				SpawnText(text[1]);
+				ngDone = true;
+			} else if (timer >= 2f && !cDone) {
+				SpawnText(text[2]);
+				cDone = true;
+			} else if (timer >= 2.5f && !oDone) {
+				SpawnText(text[3]);
+				oDone = true;
+			}
+		}
+
+	}
+
+	void SpawnText (GameObject go) {
+		GameObject newText = GameObject.Instantiate (go, go.transform.position, Quaternion.identity) as GameObject;
+		newText.transform.SetParent(canvas.transform, false);
+
+	}
+}
