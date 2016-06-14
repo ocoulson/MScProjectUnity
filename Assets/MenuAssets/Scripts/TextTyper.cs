f89046d8407343683150f4db0c1027c4e0adbbc3
ocoulson <o_coulson@hotmail.co.uk>
2016-06-14 18:06:54 +0100

    Added text typing sounds to the TextTyper script for the menus

--- Assets/MenuAssets/Scripts/TextTyper.cs
+++ Assets/MenuAssets/Scripts/TextTyper.cs
@@ -5,12 +5,15 @@ using System.Collections;
 public class TextTyper : MonoBehaviour {
 
 	public float letterPause = 0.01f;
+	public AudioClip[] sounds;
 
-	string messageText;
-    Text textComp;
-	// Use this for initialization
+	private AudioSource source { get { return GetComponent<AudioSource> (); } }
+	private string messageText;
+    private Text textComp;
+	
 	void Start () {
-
+		gameObject.AddComponent<AudioSource>();
+		source.volume = 0.5f;
 		textComp = GetComponent<Text>();
         messageText = textComp.text;
         textComp.text = "";
@@ -20,6 +23,7 @@ public class TextTyper : MonoBehaviour {
 	IEnumerator TypeText () {
          foreach (char letter in messageText.ToCharArray()) {
              textComp.text += letter;
+			 source.PlayOneShot(sounds[Random.Range(0, sounds.Length)]);
              yield return new WaitForSeconds (letterPause);
          }
      }
