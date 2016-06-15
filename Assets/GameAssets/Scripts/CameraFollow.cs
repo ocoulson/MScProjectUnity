ce147997cf0f311b6c938a634333517c52831208
ocoulson <o_coulson@hotmail.co.uk>
2016-06-15 22:24:23 +0100

    Improved animations a bit, added a script for warping between maps

---
+++ Assets/GameAssets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+	public float followSpeed = 1f;
+	Camera myCam;
+	// Use this for initialization
+	void Start () {
+		myCam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		myCam.orthographicSize = (Screen.height / 100f) / 4f;
+
+		if (target) {
+			transform.position = Vector3.Lerp(transform.position, target.position, followSpeed) + new Vector3(0,0,-10f);
+		}
+	}
+}
---
+++ Assets/GameAssets/Scripts/Warp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+	public Transform target;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		Debug.Log("Triggered");
+		col.transform.position = target.position;
+	}
+
+}
