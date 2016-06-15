3d8891ba602fa8e9a23a7eca85d56b6f4b825b57
ocoulson <o_coulson@hotmail.co.uk>
2016-06-15 17:30:54 +0100

    Animated the example character. Created the Entrance scene and used the PlayerAC animation controller. Animated the Eve character

--- Assets/ExampleAssets/Scripts/PlayerMovement.cs
+++ Assets/ExampleAssets/Scripts/PlayerMovement.cs
@@ -2,14 +2,28 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-
+	Rigidbody2D rBody;
+	Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+		rBody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		Vector2 movement_vector = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if (movement_vector != Vector2.zero) {
+			anim.SetBool ("IsWalking", true);
+			anim.SetFloat("Input_x", movement_vector.x);
+			anim.SetFloat("Input_y", movement_vector.y);
+		} else {
+			anim.SetBool ("IsWalking", false);
+		}
+
+		rBody.MovePosition(rBody.position + movement_vector * Time.deltaTime);
 	}
+
 }
