fe57b4521891e41fbea356e24793ae9e10ac23ea
ocoulson <o_coulson@hotmail.co.uk>
2016-08-10 17:42:08 +0100

    Created second Integration test, testing the interaction zone of the signpost prefab showing the Press space to read instruction when the player enters the zone. To help with testing I have made all the Ui Managers into prefabs so they can be brought into the test scene

---
+++ Assets/MoveToWaypoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToWaypoint : MonoBehaviour {
+	public GameObject waypoint;
+	private Rigidbody2D rBody;
+
+	// Use this for initialization
+	void Start () {
+		rBody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (transform.position != waypoint.transform.position) {
+			Vector2 velocity = (waypoint.transform.position - transform.position) * 2;
+			rBody.velocity = velocity;
+		} 
+	}
+}
