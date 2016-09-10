690edf02ba00e509a27374eeb55493da242c030d
ocoulson <o_coulson@hotmail.co.uk>
2016-09-10 22:25:25 +0100

    Made some small changes to menu scenes, replaced the border image with a UI border image. Added a minigames menu with access to the sorter minigame

---
+++ Assets/MinigameHolder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MinigameHolder : MonoBehaviour {
+
+	public Text text;
+	public Image icon;
+		
+	public void SetText (string newText)
+	{
+		text.text = newText;
+	}
+
+	public void SetImage(Sprite newImage) 
+	{
+		icon.sprite = newImage;
+	}
+
+	public void LoadMinigame (string name)
+	{
+		FindObjectOfType<LevelManager>().LoadLevel(name);
+	}
+}
